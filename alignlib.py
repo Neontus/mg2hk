4a3a532e049e42be6c8da34e996b0439d52798f7
Juno Kim <junokimzone@gmail.com>
2022-11-22 20:17:11 -0800

    automating from home

--- alignlib.py
+++ alignlib.py
@@ -19,47 +19,47 @@ import pick_from_LMSAL
 
 #updated align function
 def align(aia, iris, debug = False, num_max_points=3, blurFilter = 3):
-    
+
     blurredaia = lee_filter(aia, blurFilter)
     blurrediris = lee_filter(iris, blurFilter)
-    
+
     fig, ax = plt.subplots(1, 2, figsize=[10,10])
     ax[0].imshow(aia)
     ax[1].imshow(iris)
     plt.show()
     print("SIZE AFTER BLUR", aia.shape, iris.shape)
-    
-    
+
+
     # sift = cv2.SIFT_create()
     # kpsA, descsA = sift.detectAndCompute(aia, None)
     # kpsB, descsB = sift.detectAndCompute(iris, None)
-    
+
     orb = cv2.ORB_create()
     kpsA = orb.detect(aia, None)
     kpsB = orb.detect(iris, None)
-    
+
     kpsA, descsA = orb.compute(aia, kpsA)
     kpsB, descsB = orb.compute(iris, kpsB)
-    
-    
+
+
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(descsA,descsB,k=2)
-    
-    
+
+
     good = []
     for m,n in matches:
         if m.distance < 0.75*n.distance:
             good.append([m])
     good = sorted(good, key = lambda x: x[0].distance)[:num_max_points]
     matches = sorted(matches, key = lambda x: x[0].distance)[:num_max_points]
-    
+
     ptsA = np.zeros((len(matches), 2), dtype="float")
     ptsB = np.zeros((len(matches), 2), dtype="float")
     for (i, m) in enumerate(good):
         ptsA[i] = kpsA[m[0].queryIdx].pt
         ptsB[i] = kpsB[m[0].trainIdx].pt
-    
-        
+
+
     ptsA = ptsA.astype(np.float32)
     ptsB = ptsB.astype(np.float32)
     M = cv2.estimateAffinePartial2D(ptsA[:num_max_points], ptsB[:num_max_points], False)
@@ -67,9 +67,9 @@ def align(aia, iris, debug = False, num_max_points=3, blurFilter = 3):
     #print("ESTIMATE: ", M[0].shape)
     inversetra = cv2.invertAffineTransform(M[0])
     (h, w) = iris.shape[:2]
-    
-    
-    
+
+
+
     if debug:
         # matching keypoints
         matchedVis = cv2.drawMatchesKnn(aia, kpsA, iris, kpsB,
@@ -77,56 +77,56 @@ def align(aia, iris, debug = False, num_max_points=3, blurFilter = 3):
         matchedVis = imutils.resize(matchedVis, width=1000)
         cv2.imshow("Matched Keypoints", matchedVis)
         cv2.waitKey(0)
-    
+
     print("aia dimensions: ", aia.shape)
     print("done aligning")
-    
+
     return M[0], w, h
 
 
 #aligning function
-def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False, 
+def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
                 num_max_points=3, blurFilter = 3):
-    
+
     # convert to greyscale
     aiaGray = cv2.cvtColor(aia, cv2.COLOR_BGR2GRAY)
     irisGray = cv2.cvtColor(iris, cv2.COLOR_BGR2GRAY)
-    
+
     blurredaia = blur(aiaGray, blurFilter)
     blurrediris = blur(irisGray, blurFilter)
     print("blurred images")
-    
+
 #     blurredaia = aiaGray.copy()
 #     blurrediris = irisGray.copy()
-    
+
     fig, ax = plt.subplots(1, 2, figsize=[10,10])
     ax[0].imshow(blurredaia)
     ax[1].imshow(blurrediris)
     plt.show()
     print("SIZE AFTER BLUR", blurredaia.shape, blurrediris.shape)
-    
-    
+
+
     sift = cv2.SIFT_create()
     kpsA, descsA = sift.detectAndCompute(blurredaia, None)
     kpsB, descsB = sift.detectAndCompute(blurrediris, None)
-    
+
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(descsA,descsB,k=2)
-    
+
     good = []
     for m,n in matches:
         if m.distance < 0.75*n.distance:
             good.append([m])
     good = sorted(good, key = lambda x: x[0].distance)[:num_max_points]
     matches = sorted(matches, key = lambda x: x[0].distance)[:num_max_points]
-    
+
     ptsA = np.zeros((len(matches), 2), dtype="float")
     ptsB = np.zeros((len(matches), 2), dtype="float")
     for (i, m) in enumerate(good):
         ptsA[i] = kpsA[m[0].queryIdx].pt
         ptsB[i] = kpsB[m[0].trainIdx].pt
-    
-        
+
+
     ptsA = ptsA.astype(np.float32)
     ptsB = ptsB.astype(np.float32)
     M = cv2.estimateAffinePartial2D(ptsA[:num_max_points], ptsB[:num_max_points], False)
@@ -134,9 +134,9 @@ def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
     #print("ESTIMATE: ", M[0].shape)
     inversetra = cv2.invertAffineTransform(M[0])
     (h, w) = iris.shape[:2]
-    
-    
-    
+
+
+
     if debug:
         #bounding box
         a, b, c, d = (0,0), (w, 0), (w,h), (0, h)
@@ -144,7 +144,7 @@ def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
         # boxed = cv2.rectangle(aia, start, end, (0,0,255) , 2)
         # cv2.imshow("bounding box", boxed)
         # cv2.waitKey(0)
-        
+
         #matching keypoints
         matchedVis = cv2.drawMatchesKnn(blurredaia, kpsA, blurrediris, kpsB,
             matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
@@ -152,7 +152,7 @@ def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
         cv2.imshow("Matched Keypoints", matchedVis)
         cv2.waitKey(0)
 
-    
+
         #adjusted box
         #print("inverse matrix: ", inversetra)
 
@@ -162,21 +162,21 @@ def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
         #print("new points: ", newa, newb, newc, newd)
         newRectPoints = np.array([newa, newb, newc, newd], np.int32)
         newRectPoints = newRectPoints.reshape((-1, 1, 2))
-    
+
     print("aia dimensions: ", aia.shape)
-    
+
     polygoned = cv2.polylines(aia, [newRectPoints], True, (0,0,255), 3)
     cv2.imshow("PLEASE WORK", polygoned)
     cv2.waitKey(0)
-    
+
     aligned = cv2.warpAffine(aia, M[0], (w, h))
-    
+
     aligned_color = cv2.warpAffine(color_aia, M[0], (w, h))
     sv.save('quepasa.jbl.gz', aligned_color, M, w, h)
     print("RESULT SHAPE: ", aligned_color.shape)
-    
+
     cv2.imwrite(outpath, aligned_color)
-    
+
     # return the aligned aia
     return aligned, M[0], w, h
 
@@ -184,11 +184,11 @@ def align_images(color_aia, aia, iris, outpath, maxFeatures=500, debug = False,
 def applyAffine(point, affine):
     x, y = point
     newp = [x, y, 1]
-    
+
     transformed = np.matmul(affine, newp)
     transformed = transformed.astype(np.float32)
     return transformed
-    
+
 #gaussian blur function
 def blur(image, blur_filter):
     dst = cv2.GaussianBlur(image, (blur_filter, blur_filter), 0, 0)
@@ -202,7 +202,7 @@ def imgthr(image, lt, ut):
             fl[i] = True
         else:
             fl[i] = False
-    
+
     return np.reshape(fl, image.shape)
 
 
@@ -245,13 +245,13 @@ def get_top_n(img, n):
     sort_flat = np.sort(img_flat)
     l = int(len(sort_flat)*(1-n))
     thr = sort_flat[l]
-    
+
     return thr
 
 def manual_align(aiao, iriso):
     aia = cv2.cvtColor(aiao, cv2.COLOR_GRAY2BGR)
     iris = cv2.cvtColor(iriso, cv2.COLOR_GRAY2BGR)
-    
+
     def detectClick(event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
             if params == "iris":
@@ -269,7 +269,7 @@ def manual_align(aiao, iriso):
     kpsa = [] #aia keypoints
     nkpsa = []
     nkpsi = []
-    
+
 
     print("[CHECKPOINT] select keypoints")
     cv2.imshow("IRIS keypoint select", iris)
@@ -279,21 +279,21 @@ def manual_align(aiao, iriso):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     print("[UPDATE]: how many keypoints - ", len(kpsi), len(kpsa))
-    
+
     for kp in kpsa:
         nkpsa.append(kp.pt)
-    
+
     for kp in kpsi:
         nkpsi.append(kp.pt)
-    
+
     nkpsa = np.array(nkpsa)
     nkpsi = np.array(nkpsi)
-       
+
     numkp = len(kpsi)
-    
+
     ptsA = np.zeros((numkp, 2), dtype="float")
     ptsB = np.zeros((numkp, 2), dtype="float")
-    
+
     for i in range(numkp):
         ptsA[i] = nkpsa[i].astype(np.float32)
         ptsB[i] = nkpsi[i].astype(np.float32)
