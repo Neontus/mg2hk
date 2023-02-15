624917e70a0e71e7fa60213289d7164b11341b5a
Neontus <junokimzone@gmail.com>
2023-02-14 18:59:30 -0800

    finished closest_time, starting AIA cropping with conversion

--- aiarasterfunc.py
+++ aiarasterfunc.py
@@ -2,32 +2,50 @@ import pick_from_LMSAL
 import my_fits
 import numpy as np
 import rebin
+import os
 
+from dateutil.parser import parse
 from iris_lmsalpy import extract_irisL2data as ei
 
-def new_load(obsid):
+#from iris_lmsalpy import hcr2fits
+#query_text = 'https://www.lmsal.com/hek/hcr?cmd=search-events3&outputformat=json&startTime=2013-07-20T00:00&stopTime=2023-01-20T00:00&minnumRasterSteps=100&hasData=true&minxCen=-400&maxxCen=400&minyCen=-400&maxyCen=400&hideMostLimbScans=true&limit=200'
+#list_urls = hcr2fits.get_fits(query_text)
+
+iris_filename = "/Users/jkim/Desktop/mg2hk/data/iris_l2_20230103_194208_3610108077_raster_t000_r00000.fits"
+aia_folder = "/Users/jkim/Desktop/mg2hk/data/iris_l2_20230103_194208_3610108077_SDO/"
+
+def closest_time(iris_time, aia_times):
+	differences = [abs(parse(iris_time) - parse(aia_time)) for aia_time in aia_times]
+	# print("time difference: ", min(differences))
+	return differences.index(min(differences))
+
+def new_load():
 	numraster = 0
-	print("testing with: (OBSID - {})".format(obsid))
-	try:
-		iris_file = pick_from_LMSAL.obsid_raster(obsid, raster=numraster)
-		aia_file = pick_from_LMSAL.obsid_raster2aia(obsid)
+	#print("testing with: (OBSID - {})".format(obsid))
+	# try:
+	# 	iris_file = pick_from_LMSAL.obsid_raster(obsid, raster=numraster)
+	# 	aia_file = pick_from_LMSAL.obsid_raster2aia(obsid)
+	#
+	# except:
+	# 	print("error")
+	# 	exit(0)
 
-	except:
-		print("error")
-		exit(0)
+	iris_file = iris_filename
 
-	print("1", iris_file)
-	print("2", aia_file)
-
-	hdr_iris_data = ei.only_header(iris_file[0])
-	aux_hdr_iris_data = ei.only_header(iris_file[0], extension=1)
+	hdr_iris_data = ei.only_header(iris_file)
+	aux_hdr_iris_data = ei.only_header(iris_file, extension=1)
 	xcen_iris = hdr_iris_data['XCEN']
 	ycen_iris = hdr_iris_data['YCEN']
 	xscl_iris = aux_hdr_iris_data['CDELT3']
 	yscl_iris = aux_hdr_iris_data['CDELT2']
 
-	iris_raster = ei.load(iris_file[0])
+	iris_raster = ei.load(iris_file)
 	extent_hx_hy = iris_raster.raster['Mg II k 2796'].extent_heliox_helioy
+
+	stepx = iris_raster.raster['Mg II k 2796'].XCENIX
+	stepy = iris_raster.raster['Mg II k 2796'].YCENIX
+	iris_times = iris_raster.raster['Mg II k 2796'].date_time_acq_ok
+
 	mgii = iris_raster.raster['Mg II k 2796'].data
 	sel_wl = ei.get_ori_val(iris_raster.raster['Mg II k 2796'].wl, [2794.73])
 	# if opt2 == True: sel_wl = ei.get_ori_val(iris_raster.raster['Mg II k 2796'].wl, [2795.99])
@@ -41,9 +59,29 @@ def new_load(obsid):
 
 	hiris, wiris = new_iris_data.shape
 	extent_iris = [xcen_iris - wiris / 2, xcen_iris + wiris / 2, ycen_iris - hiris / 2, ycen_iris + hiris / 2]
-	print(extent_iris)
+	# print(extent_iris)
 
-new_load('20221026_041953_3600609177')
+	aia_times = []
+	aia_imgs = []
+
+	for aia_file in os.listdir(aia_folder):
+		aia_img, aia_header = my_fits.read(aia_folder+aia_file)
+		aia_time = aia_header['DATE_OBS']
+		aia_times.append(aia_time)
+		aia_imgs.append(aia_img)
+
+	time_dict = dict(zip(iris_times, [closest_time(t, aia_times) for t in iris_times]))
+
+	for i, t in enumerate(time_dict):
+		aia = aia_imgs[i]
+
+
+
+
+new_load()
+
+
+
 
 #pick_from_LMSAL.obsid_raster2aia('20221026_041953_3600609177')
 
